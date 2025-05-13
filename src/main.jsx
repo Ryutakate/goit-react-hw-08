49f15f2f7a62d0b9e86f8d9b969d14588b28841f
Ryutakate <magegavladislav@gmail.com>
2025-05-14 01:11:42 +0300

    add src components and app.jsx v-3

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import './index.css';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-// import { BrowserRouter } from 'react-router-dom';
 import { HashRouter } from 'react-router-dom';
 
 
@@ -14,10 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {/* <BrowserRouter>
-          <App />
-        </BrowserRouter> */}
-
         <HashRouter>
           <App />
         </HashRouter>
