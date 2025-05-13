beb2b784f677d4a5318d199d14c4ceb396b57f60
Ryutakate <magegavladislav@gmail.com>
2025-05-14 01:20:17 +0300

    add src components and app.jsx v-3

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/goit-react-hw-08/' : '/',
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [react()],
 });
