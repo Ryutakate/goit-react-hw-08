a4aba09a0bfa002836a7d0e01245c6d083cd0b10
Ryutakate <magegavladislav@gmail.com>
2025-05-14 20:55:03 +0300

    add src components and app.jsx v-3

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contacts/operations';
-import { selectFilteredContacts } from '../../redux/contacts/slice';
+import { selectFilteredContacts } from '../../redux/contacts/selectors';
 import styles from './ContactList.module.css';
 
 const ContactList = () => {
