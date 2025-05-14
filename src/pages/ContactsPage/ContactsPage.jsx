703ad25e29d8c9189a6c3edd119f30201b6bd65b
Ryutakate <magegavladislav@gmail.com>
2025-05-14 20:52:10 +0300

    add src components and app.jsx v-3

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,3 +1,24 @@
-export default function ContactsPage() {
-    return <h1>Contacts Page</h1>;
-}  
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../../redux/contacts/operations';
+import ContactForm from '../../components/ContactForm/ContactForm';
+import ContactList from '../../components/ContactList/ContactList';
+
+const ContactsPage = () => {
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
+    
+    return (
+        <div>
+            <h2>Your Contacts</h2>
+            <ContactForm />
+            <Filter />
+            <ContactList />
+        </div>
+    );
+};
+
+export default ContactsPage;
